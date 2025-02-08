f23946aad55d677853ec9f21cb768af059681ba2
c7yrus <42728641+c7yrus@users.noreply.github.com>
2025-02-08 20:16:56 +1100

    evals for all the agents

---
+++ evals/experiments/generateImage.eval.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+
+import  runLLM  from '../../src/llm'
+import { generateImageToolDefinition } from '../../src/tools/generateImage'
+import { runEval } from '../evalTools'
+import { ToolCallMatch } from '../scorers'
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: {
+        name: toolName,
+      },
+    },
+  ],
+})
+
+runEval('generateImage', {
+  task: (input) =>
+    runLLM({
+      messages: [{ role: 'user', content: input }],
+      tools: [generateImageToolDefinition],
+    }),
+  data: [
+    {
+      input: 'Generate an image of a sunset',
+      expected: createToolCallMessage(generateImageToolDefinition.name),
+    },
+    {
+      input: 'take a photo of the sunset',
+      expected: createToolCallMessage(generateImageToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+})
